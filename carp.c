db07f4d41999a3471636570a7ee0a54908aa75c9
Jake Stannix <jakestannix@gmail.com>
2023-12-30 14:27:45 -0700

    basic build system in place

--- carp.c
+++ carp.c
@@ -1,112 +1,168 @@
 #include <stdbool.h>
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+bool is_newer(char *to_check, char *older) {
+  struct stat src_attr, prog_attr;
+  if (stat(older, &prog_attr) == -1) {
+    perror("could not get process stats");
+    exit(EXIT_FAILURE);
+  }
+  if (stat(to_check, &src_attr) == -1) {
+    perror("could not get src file stats");
+    exit(EXIT_FAILURE);
+  }
+  if (prog_attr.st_mtim.tv_sec < src_attr.st_mtim.tv_sec) {
+    return true;
+  }
+  return false;
+}
+
+bool chechrebuild(int argc, char *argv[]) {
+
+  char *file_name = __FILE__;
+
+  if (is_newer(file_name, argv[0])) {
+    printf("[BUILD] rebuilding carp\n.");
+    system("cc -o carp carp.c");
+    system("./carp");
+    return true;
+  }
+  return false;
+}
 
 typedef struct {
-  char *source_path; /**< The reletive path from the build directory of the
-                        source file. */
-  char *name;        /**< The object/binary name */
-  bool bin;          /**< Binary or object flag */
-  struct args {
-    int len;
-    char **ptr;
-  } args;       /**< Compiliation arguments */
-  char *output; /**< Output file for compilition. NULL if compilation has not
-                   yet taken place. */
-} Compile;
+  int count;
+  int capacity;
+  char *string;
+} StringBuilder;
 
-/** \relates Compile
- * Initialize a compile object.
- * @param[out] c            The Compile struct to be instantiated.
- * @param[in]  rel_src_path The path of the source file reletive to the src/
- * dir.
- * @param[in]  name         The name of the object to be created.
- * @param[in]  args         Optional additional compile arguments to be appended
- * to default compile args.
- * @param      arg_len      The lenth of the args; Is not checked if args is
- * NULL.
- * @param      bin          Flag for binary compilation or object compilation.
- * @returns                 0 on success, -1 on failure.
+/**
+ * @returns true for success; false for failure.
  */
-int compile_init(Compile *c, char *rel_src_path, char *name, char *args[],
-                 int arg_len, bool bin) {
-  c->source_path = c->name = c->output = NULL;
-  c->args.len = -1;
-  c->args.ptr = NULL;
-  c->bin = false;
+bool sb_append(StringBuilder *sb, char *append) {
+  int old_cap = sb->capacity;
+  sb->capacity = old_cap + strlen(append) + 2; // add 1 for space.
+  char *buff = malloc(sb->capacity);
+  if (buff == NULL)
+    return false;
+  if (sb->count != 0) {
+    sprintf(buff, "%s %s", sb->string, append);
+    free(sb->string);
+  } else {
+    buff = strcpy(buff, append);
+  }
+  sb->string = buff;
+  sb->count = sb->capacity;
+  return true;
+}
+bool sb_append_many(StringBuilder *sb, char *append[], int len) {
+  for (int i = 0; i < len; i++) {
+    if (!sb_append(sb, append[i]))
+      return false;
+  }
+  return true;
+}
+
+typedef struct {
+  char *source_file; /**< Path to source file */
+  char *name;        /**< binary name */
+  int cflag_len;
+  char **cflags;     /**< optional cflags */
+  char *output_file; /**< output file path; if NULL build has yet to have taken
+                        place. */
+  bool binary;       /**< build as a binary or object */
+} Compile;
+bool compile_init(Compile *c, char *name, char *source_file, char **cflags,
+                  int cflags_len, bool binary) {
+  *c = (Compile){NULL, NULL, 0, NULL, NULL, false};
 
 #define SRC "src/"
-  c->source_path =
-      malloc(sizeof(char) * (strlen(rel_src_path) + strlen(SRC) + 1));
-  if (c->source_path == NULL)
+  c->source_file = malloc(strlen(source_file) + strlen(SRC) + 1);
+  if (c->source_file == NULL)
     goto errdefer;
-  sprintf(c->source_path, "%s%s", SRC, rel_src_path);
+  if (sprintf(c->source_file, "%s%s", SRC, source_file) == -1)
+    goto errdefer_sprintf;
 #undef SRC
-
-  c->name = malloc(strlen(name) * sizeof(char));
+  c->name = malloc(strlen(name) + 1);
   if (c->name == NULL)
-    goto errdefer_src_path;
-  memcpy(c->name, name, strlen(name));
+    goto errdefer_sprintf;
+  c->name = memcpy(c->name, name, strlen(name));
 
-  if (args != NULL) {
-    c->args.ptr = malloc(arg_len * sizeof(char *));
-    if (c->args.ptr == NULL)
-      goto errdefer_name;
-    for (int i = 0; i < arg_len; i++) {
-      c->args.ptr[i] = NULL;
-    }
-
-    for (int i = 0; i < arg_len; i++) {
-      c->args.ptr[i] = malloc(sizeof(char) * strlen(args[i]));
-      if (c->args.ptr[i] == NULL)
-        goto errdefer_args;
-      c->args.len = i;
-      memcpy(c->args.ptr[i], args[i], strlen(args[i]));
-    }
-    c->args.len++;
+  if (binary) {
+    c->binary = true;
   }
 
-  if (bin)
-    c->bin = true;
-
-  goto defer;
-errdefer_args:
-  if (args != NULL) {
-    for (int i = c->args.len - 1; i >= 0; i--) {
-      free(c->args.ptr[i]);
+  if (cflags != NULL) {
+    c->cflags = malloc(sizeof(char *) * cflags_len);
+    if (c->cflags == NULL)
+      goto errdefer_name;
+    for (int i = 0; i < cflags_len; i++) {
+      c->cflags[i] = NULL;
     }
-    free(c->args.ptr);
+
+    for (int i = 0; i < cflags_len; i++) {
+      c->cflags[i] = malloc(strlen(cflags[i]));
+      if (c->cflags[i] == NULL)
+        goto errdefer_arg_vals;
+      c->cflag_len++;
+      strcpy(c->cflags[i], cflags[i]);
+    }
+  }
+  goto defer;
+
+errdefer_arg_vals:
+  if (c->cflags != NULL) {
+    for (int i = c->cflag_len - 1; i >= 0; i--) {
+      free(c->cflags[i]);
+    }
+    free(c->cflags);
   }
 errdefer_name:
   free(c->name);
-errdefer_src_path:
-  free(c->source_path);
+errdefer_sprintf:
+  free(c->source_file);
 errdefer:
-  return -1;
+  return false;
+
 defer:
-  return 0;
+  return true;
 }
-void compile_free(Compile *c) {
-  if (c->args.ptr != NULL) {
-    for (int i = c->args.len - 1; i >= 0; i--) {
-      free(c->args.ptr[i]);
-    }
-    free(c->args.ptr);
+
+bool compile_build(Compile *c) {
+  StringBuilder sb = {0, 0, NULL};
+
+  char *args[] = {
+      "cc",
+      "-o",
+      c->name,
+  };
+  if (!sb_append_many(&sb, args, sizeof(args) / sizeof(char *)))
+    return false;
+  if (c->cflag_len > 0) {
+    if (!sb_append_many(&sb, c->cflags, c->cflag_len))
+      return false;
   }
 
-  free(c->name);
+  if (!sb_append(&sb, c->source_file))
+    return false;
 
-  free(c->source_path);
+  printf("[BUILD] %s: %s\n", c->name, sb.string);
+  if (system(sb.string) == -1)
+    return false;
+  c->output_file = c->name;
+  return true;
 }
+
 int main(int argc, char *argv[]) {
+  if (chechrebuild(argc, argv)) {
+    exit(EXIT_SUCCESS);
+  }
   Compile c;
-  if (compile_init(&c, "main.c", "hello", NULL, 0, true) == -1)
-    return 1;
-  printf("Compile{\n");
-  printf("\tsource_path: %s,\n", c.source_path);
-  printf("\tname: %s,\n", c.name);
-  printf("}\n");
-  compile_free(&c);
+  if (!compile_init(&c, "hello", "main.c", NULL, 0, true))
+    exit(EXIT_FAILURE);
+  compile_build(&c);
 }
