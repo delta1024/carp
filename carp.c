926d3e42af7dc785ef95317b5528d7e0afec8c3f
Jake Stannix <jakestannix@gmail.com>
2023-12-31 13:42:02 -0700

    added dependencies and header tracking

--- carp.c
+++ carp.c
@@ -13,35 +13,57 @@
 #define init_obj(c, name, path)                                                \
   try(compile_init((c), (name), (path), NULL, 0, true))
 
-result_t build(int argc, char *argv[]) {
+enum CARP_ARGS_RESULT {
+  CARP_ARGS_NO_RUN,
+  CARP_ARGS_RAN,
+};
 
-  if (argc >= 2 && strcmp("clean", argv[1]) == 0) {
-    DIR *ck = opendir("./build");
-    if (ck != NULL) {
-      closedir(ck);
-    carp_log(CARP_LOG_INFO, "rm -r ./build");
-    system("rm -r ./build");      
-    }
+enum CARP_ARGS_RESULT parse_args(int argc, char *argv[]) {
+  if  (argc >= 2 && strcmp("clean", argv[1]) == 0) {
+      DIR *ck = opendir("./build");
+      if (ck != NULL) {
+	closedir(ck);
+	carp_log(CARP_LOG_INFO, "rm -r ./build");
+	system("rm -r ./build");      
+      }
 
     if (argc == 3 && strcmp("all", argv[2]) == 0) {
       carp_log(CARP_LOG_INFO, "rm ./carp");
       system("rm ./carp");
     }
+
     if (argc == 3 && strcmp("rebuild", argv[2]) == 0) {
       carp_log(CARP_LOG_INFO, "./carp");
       system("./carp");
-      return CARP_RESULT_OK;
+ 
     }
     
+    return CARP_ARGS_RAN;
+  }
+  return CARP_ARGS_NO_RUN;
+
+}
+
+result_t build(int argc, char *argv[]) {
+  if (parse_args(argc, argv) == CARP_ARGS_RAN) {
     return CARP_RESULT_OK;
   }
-    IMPL_REBUILD(argv);
+
+  IMPL_REBUILD(argv);
   result_t result = CARP_RESULT_OK;
 
-  Compile c;
+  Compile c, t;
   try(compile_init(&c, "hello", "src/main.c", NULL, 0, false));
+
+  init_obj(&t, "temp", "src/temp.c");
+  try(headers_append(&t.headers, "src/temp.h"));
+  
+  try(deps_append(&c.deps, &t));
   try(compile_run(&c));
 
   return result;
 }
+
+
+
 
--- src/carp.h
+++ src/carp.h
@@ -2,6 +2,7 @@
 #define CARP_H_
 
 #include <dirent.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +10,6 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdarg.h>
 
 /************************************************************
  *                       carp.h                             *
@@ -45,20 +45,38 @@ typedef struct {
 result_t sb_append(StringBuilder *sb, char *arg);
 result_t sb_append_many(StringBuilder *sb, char **args, int args_len);
 
-
-
 /************************************************************
  *                    compile.h                             *
  ************************************************************/
+typedef struct Compile Compile;
 
-typedef struct Compile {
+typedef struct Deps {
+  int count;
+  int cap;
+  Compile **ptr;
+} Deps;
+
+typedef struct Headers {
+  int count;
+  int cap;
+  char **ptr;
+} Headers;
+
+struct Compile {
   char *name, *output_file, *source_file, **args;
+  Deps deps;
+  Headers headers;
   bool is_object;
   int args_len;
-} Compile;
+};
 
-result_t compile_init(Compile *c, char *name, char *source_file, char *args[], int arg_len, bool is_object);
+result_t compile_init(Compile *c, char *name, char *source_file, char *args[],
+                      int arg_len, bool is_object);
+bool compile_needs_rebuild(Compile *c);
 result_t compile_run(Compile *c);
+result_t deps_append(Deps* d, Compile *c);
+result_t headers_append(Headers *d, char *path);
+
 #ifdef CARP_IMPL
 /************************************************************
  *                      carp.c                              *
@@ -104,13 +122,13 @@ result_t sb_append(StringBuilder *sb, char *arg) {
   } else {
     str_len = 0;
   }
-  sb->ptr = (char*)realloc(sb->ptr, arg_len + str_len + 3);
+  sb->ptr = (char *)realloc(sb->ptr, arg_len + str_len + 3);
   if (sb->ptr == NULL) {
     return CARP_ERR_NOMEM;
   }
   if (str_len != 0) {
     sb->ptr[str_len] = ' ';
-    strcpy(&sb->ptr[str_len + 1], arg);    
+    strcpy(&sb->ptr[str_len + 1], arg);
   } else {
     strcpy(sb->ptr, arg);
   }
@@ -132,10 +150,40 @@ result_t sb_append_many(StringBuilder *sb, char **args, int args_len) {
 /************************************************************
  *                    compile.c                             *
  ************************************************************/
+
 // NOLINTNEXTLINE(misc-definitions-in-headers)
-result_t compile_init(Compile *c, char *name, char *source_file, char *args[], int arg_len, bool is_object) {
+result_t deps_append(Deps *d, Compile *c) {
+  if (d->cap < d->count + 1) {
+    int old_cap = d->cap;
+    d->cap = old_cap < 8 ? 8 : old_cap * 2;
+    d->ptr = (Compile**)realloc(d->ptr, sizeof(Compile*) * d->cap);
+    if (d->ptr == NULL){
+      return CARP_ERR_NOMEM;
+    }
+  }
+  d->ptr[d->count++] = c;
+  return CARP_RESULT_OK;
+}
+
+// NOLINTNEXTLINE(misc-definitions-in-headers)
+result_t headers_append(Headers *h, char *path) {
+  if (h->cap < h->count + 1) {
+    int old_cap = h->cap;
+    h->cap = old_cap < 8 ? 8 : old_cap * 2;
+    h->ptr = (char**)realloc(h->ptr, sizeof(char*) * h->cap);
+    if (h->ptr == NULL){
+      return CARP_ERR_NOMEM;
+    }
+  }
+  h->ptr[h->count++] = path;
+  return CARP_RESULT_OK;
+}
+
+// NOLINTNEXTLINE(misc-definitions-in-headers)
+result_t compile_init(Compile *c, char *name, char *source_file, char *args[],
+                      int arg_len, bool is_object) {
   result_t result;
-  *c = (Compile){NULL, NULL, NULL, NULL, false, 0};
+  *c = (Compile){NULL, NULL, NULL, NULL, (Deps){0, 0, NULL}, (Headers){0,0,NULL}, false, 0};
 
   c->name = malloc(strlen(name) + 1);
   if (c->name == NULL) {
@@ -143,14 +191,14 @@ result_t compile_init(Compile *c, char *name, char *source_file, char *args[], i
     goto no_dealloc;
   }
   strcpy(c->name, name);
-  
+
   c->source_file = malloc(strlen(source_file) + 1);
   if (c->source_file == NULL) {
     result = CARP_ERR_NOMEM;
     goto free_name;
   }
   strcpy(c->source_file, source_file);
-  
+
   if (is_object) {
     c->is_object = true;
     c->output_file = malloc(strlen(name) + 3 + strlen("build/"));
@@ -160,7 +208,7 @@ result_t compile_init(Compile *c, char *name, char *source_file, char *args[], i
     }
     sprintf(c->output_file, "build/%s.o", name);
   } else {
-    c->output_file  = malloc(strlen(name) + 1 + strlen("build/"));
+    c->output_file = malloc(strlen(name) + 1 + strlen("build/"));
     if (c->output_file == NULL) {
       result = CARP_ERR_NOMEM;
       goto free_dest;
@@ -168,16 +216,16 @@ result_t compile_init(Compile *c, char *name, char *source_file, char *args[], i
     sprintf(c->output_file, "build/%s", name);
   }
   if (args != NULL) {
-    c->args = malloc(sizeof(char*) * arg_len);
-      if (c->args == NULL) {
-	result = CARP_ERR_NOMEM;
-	goto free_dest;
-      }
+    c->args = malloc(sizeof(char *) * arg_len);
+    if (c->args == NULL) {
+      result = CARP_ERR_NOMEM;
+      goto free_dest;
+    }
     for (int i = 0; i < arg_len; i++) {
-      char *buf =  malloc(strlen(args[i]) + 1);
+      char *buf = malloc(strlen(args[i]) + 1);
       if (buf == NULL) {
-	result = CARP_ERR_NOMEM;
-	goto free_args;
+        result = CARP_ERR_NOMEM;
+        goto free_args;
       }
       strcpy(c->args[i], buf);
       c->args_len++;
@@ -186,46 +234,83 @@ result_t compile_init(Compile *c, char *name, char *source_file, char *args[], i
 
   result = CARP_RESULT_OK;
   goto no_dealloc;
-  
- free_args:
-  if (c->args != NULL){
+
+free_args:
+  if (c->args != NULL) {
     for (int i = c->args_len - 1; i >= 0; i--) {
       free(c->args[i]);
     }
-    free(c->args);    
+    free(c->args);
   }
 
- free_dest:
+free_dest:
   free(c->output_file);
   c->output_file = NULL;
- free_name:
+free_name:
   free(c->name);
   c->name = NULL;
-  
- no_dealloc:
+
+no_dealloc:
   return result;
 }
 
+// NOLINTNEXTLINE(misc-definitions-in-headers)
+bool compile_needs_rebuild(Compile *c) {
+  if (!(access(c->output_file, F_OK) == 0 &&
+	 is_newer(c->output_file, c->source_file)))
+    return true;
+
+  for (int i = 0; i < c->headers.count; i++) {
+    if (!is_newer(c->output_file, c->headers.ptr[i]))
+      return true;
+  }
+  
+  for (int i = 0; i < c->deps.count; i++) {
+    if (compile_needs_rebuild(c->deps.ptr[i]))
+      return true;
+  }
+
+
+  return false;
+}
+
+// NOLINTNEXTLINE(misc-definitions-in-headers)
 result_t compile_run(Compile *c) {
-  DIR* build_dir = opendir("build");
+
+  DIR *build_dir = opendir("build");
   if (build_dir == NULL) {
     carp_log(CARP_LOG_INFO, "mkdir build");
     system("mkdir build");
   } else {
     closedir(build_dir);
   }
-  StringBuilder sb = {0, NULL};
-  
-  if (access(c->output_file, F_OK) == 0 && is_newer(c->output_file, c->source_file)) {
+
+  if (!compile_needs_rebuild(c)) {
     return CARP_RESULT_OK;
   }
+
+  result_t result;
+  for (int i = 0; i < c->deps.count; i++) {
+    if ((result = compile_run(c->deps.ptr[i])) != CARP_RESULT_OK) {
+      return result;
+    }
+  }
+  
+  StringBuilder sb = {0, NULL};  
   char *args[] = {
       "cc",
       "-o",
       c->output_file,
   };
-  if (sb_append_many(&sb, args, sizeof(args) / sizeof(char *)) != CARP_RESULT_OK)
+  
+  if (sb_append_many(&sb, args, sizeof(args) / sizeof(char *)) !=
+      CARP_RESULT_OK)
     return CARP_ERR_NOMEM;
+
+  if (c->is_object) {
+    if (sb_append(&sb, "-c") != CARP_RESULT_OK)
+      return CARP_ERR_NOMEM;
+  }
   
   if (c->args_len > 0) {
     if (sb_append_many(&sb, c->args, c->args_len) != CARP_RESULT_OK)
@@ -235,7 +320,12 @@ result_t compile_run(Compile *c) {
   if (sb_append(&sb, c->source_file) != CARP_RESULT_OK)
     return CARP_ERR_NOMEM;
 
-  carp_log(CARP_LOG_COMPILE,  "%s", sb.ptr);
+  for (int i = 0; i < c->deps.count; i++) {
+    if (sb_append(&sb, c->deps.ptr[i]->output_file) != CARP_RESULT_OK)
+      return CARP_ERR_NOMEM;
+  }
+
+  carp_log(CARP_LOG_COMPILE, "%s", sb.ptr);
   if (system(sb.ptr) == -1)
     return CARP_ERR_COMPILE;
 
@@ -246,20 +336,18 @@ result_t compile_run(Compile *c) {
  *                      main.c                              *
  ************************************************************/
 
-
-#define IMPL_REBUILD(argv)						\
-  do {									\
-    char this_file[] = __FILE__;					\
-    char *binary = argv[0];						\
-    if (is_newer(this_file, binary)					\
-	|| is_newer("./src/carp.h", binary)) {				\
-      carp_log(CARP_LOG_INFO,"rebuilding carp");			\
-      carp_log(CARP_LOG_COMPILE, "cc -o carp carp.c");			\
-      system("cc -o carp carp.c");					\
-      system("./carp");							\
-      return CARP_RESULT_OK;						\
-    }									\
-  } while(false) 
+#define IMPL_REBUILD(argv)                                                     \
+  do {                                                                         \
+    char this_file[] = __FILE__;                                               \
+    char *binary = argv[0];                                                    \
+    if (is_newer(this_file, binary) || is_newer("./src/carp.h", binary)) {     \
+      carp_log(CARP_LOG_INFO, "rebuilding carp");                              \
+      carp_log(CARP_LOG_COMPILE, "cc -o carp carp.c");                         \
+      system("cc -o carp carp.c");                                             \
+      system("./carp");                                                        \
+      return CARP_RESULT_OK;                                                   \
+    }                                                                          \
+  } while (false)
 
 result_t build(int argc, char *argv[]);
 
@@ -267,7 +355,7 @@ result_t build(int argc, char *argv[]);
 int main(int argc, char *argv[]) {
   switch (build(argc, argv)) {
   case CARP_ERR_NOMEM:
-    carp_log(CARP_LOG_ERR,"OOM");
+    carp_log(CARP_LOG_ERR, "OOM");
     exit(EXIT_FAILURE);
     break;
   case CARP_ERR_COMPILE:
